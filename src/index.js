c3f5a1bc919a1e91e1407f1f9e1bb2c952aa6474
andrefmandrade <andrefmandrade@outlook.com>
2020-06-07 08:47:40 -0300

    script dev fix

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import express from 'express';
-import helmet from 'helmet';
-import cors from 'cors';
-import routes from './routes';
-import 'dotenv/config';
+import express from "express";
+import helmet from "helmet";
+import cors from "cors";
+import routes from "./routes";
+import "dotenv/config";
 
 const app = express();
 
@@ -11,6 +11,6 @@ app.use(express.json());
 app.use(routes);
 app.use(helmet());
 
-app.listen(process.env.PORT || 8080, () => {
-  console.log(`Server running in port ${process.env.PORT || 8080}`);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server running in port ${process.env.PORT || 3000}`);
 });
