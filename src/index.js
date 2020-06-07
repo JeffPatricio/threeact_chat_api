2919301263d90317d7e462536da7e92d72d595e0
Jefferson Patrício <jeffersonpatricio1@gmail.com>
2020-06-07 10:07:57 -0300

    configuração socket io

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import express from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
-import routes from './routes';
 import 'dotenv/config';
+import routes from './routes';
 
 const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(routes);
 app.use(helmet());
+app.listen(process.env.PORT || 8080, () => console.log(`Server running in port ${process.env.PORT || 8080}`));
 
-app.listen(process.env.PORT || 8080, () => {
-  console.log(`Server running in port ${process.env.PORT || 8080}`);
-});
